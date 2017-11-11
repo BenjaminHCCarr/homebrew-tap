25c51b02c4fd02c311d225349a4501e6cd86beba
Benjamin H C Carr <bencarr+git@gmail.com>
2017-11-11 12:00:03 -0500

    Did an audit --fix with cask by mistake

--- Formula/toralpha.rb
+++ Formula/toralpha.rb
@@ -1,18 +1,18 @@
 class Toralpha < Formula
-  desc 'Anonymizing overlay network for TCP'
-  homepage 'https://www.torproject.org/'
-  url 'https://tor.eff.org/dist/tor-0.3.2.4-alpha.tar.gz'
-  mirror 'https://dist.torproject.org/tor-0.3.2.4-alpha.tar.gz'
-  sha256 'c3ea7826b783d4357a624248d8448480f27838ceb3c1bba02a4a0e5d36564fba'
+  desc "Anonymizing overlay network for TCP"
+  homepage "https://www.torproject.org/"
+  url "https://tor.eff.org/dist/tor-0.3.2.4-alpha.tar.gz"
+  mirror "https://dist.torproject.org/tor-0.3.2.4-alpha.tar.gz"
+  sha256 "c3ea7826b783d4357a624248d8448480f27838ceb3c1bba02a4a0e5d36564fba"
 
   bottle :unneeded
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libevent'
-  depends_on 'openssl'
-  depends_on 'libscrypt'
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on "openssl"
+  depends_on "libscrypt"
 
-  conflicts_with 'tor'
+  conflicts_with "tor"
 
   def install
     args = "
@@ -21,14 +21,14 @@ class Toralpha < Formula
              --prefix=#{prefix}
              --sysconfdir=#{etc}
              --localstatedir=#{var}
-             --with-openssl-dir=#{Formula['openssl'].opt_prefix}
+             --with-openssl-dir=#{Formula["openssl"].opt_prefix}
            "
 
-    system './configure', *args
-    system 'make', 'install'
+    system "./configure", *args
+    system "make", "install"
   end
 
-  plist_options manual: 'tor'
+  plist_options :manual => "tor"
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
@@ -58,9 +58,9 @@ class Toralpha < Formula
 
   test do
     pipe_output("script -q /dev/null #{bin}/tor-gencert --create-identity-key", "passwd\npasswd\n")
-    assert_predicate testpath / 'authority_certificate', :exist?
-    assert_predicate testpath / 'authority_signing_key', :exist?
-    assert_predicate testpath / 'authority_identity_key', :exist?
+    assert_predicate testpath / "authority_certificate", :exist?
+    assert_predicate testpath / "authority_signing_key", :exist?
+    assert_predicate testpath / "authority_identity_key", :exist?
   end
 
   def caveats; <<-EOS.undent
