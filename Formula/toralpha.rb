4d5246f0b6ef14d82fb44283d60a8aa4606fc7d6
Benjamin H C Carr <bencarr+git@gmail.com>
2018-04-18 14:47:01 -0400

    bump toralpha to 0.3.3.5RC and fix <<~EOS

--- Formula/toralpha.rb
+++ Formula/toralpha.rb
@@ -35,7 +35,7 @@ class Toralpha < Formula
 
   plist_options :manual => "tor"
 
-  def plist; <<-EOS
+  def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
