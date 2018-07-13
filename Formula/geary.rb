a88c3a8aaba69ad2b78497c1d88cad6a409a7a8b
Benjamin H C Carr <bencarr+git@gmail.com>
2018-07-13 13:56:30 -0400

    Still working on geary

--- Formula/geary.rb
+++ Formula/geary.rb
@@ -1,8 +1,8 @@
 class Geary < Formula
   desc "Is an email application built around conversations"
   homepage "https://wiki.gnome.org/Apps/Geary"
-  url "https://git.gnome.org/browse/geary/snapshot/geary-0.12.1.zip"
-  sha256 "5aaffb608ec0704dd55ad59a6b0cb92300456a7e0384ef1399f3a1e9f4db1036"
+  url "https://gitlab.gnome.org/GNOME/geary/-/archive/geary-0.12.2/geary-geary-0.12.2.tar.gz"
+  sha256 "6cf269fb0e2d813ea0d574093de9de05c98ae39ba27bba74ada949f88de11e8d"
 
   bottle :unneeded
   depends_on "cmake" => :build
@@ -10,25 +10,27 @@ class Geary < Formula
   depends_on "pkg-config" => :build
   depends_on "desktop-file-utils"
   depends_on "enchant"
-  depends_on "gcr"
+  # depends_on "gcr"
+  depends_on "gettext"
   depends_on "glib"
   depends_on "gmime"
   depends_on "gnome-doc-utils"
   depends_on "gobject-introspection"
   depends_on "gtk+3"
   depends_on "libcanberra"
+  depends_on "libgcrypt"
   depends_on "libgee"
   depends_on "libnotify"
   depends_on "libsecret"
   depends_on "libxml2"
-  depends_on "sqlite"
   depends_on "vala"
-  # webkitgtk4-devel
 
   def install
     system "./configure", "--disable-desktop-update",
+                          "--disable-desktop-validate",
                           "--disable-icon-update",
                           "--disable-contractor",
+                          "--disable-schemas-compile",
                           "--prefix=#{prefix}"
     system "cmake", *std_cmake_args
     system "make", "install"
